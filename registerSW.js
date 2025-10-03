d2dfdf80371050792e113e2bb67162a396653ea9
trsdn <torsten.mahr@gmail.com>
2025-10-03 16:32:17 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/learning-platform/sw.js', { scope: '/learning-platform/' })})}
